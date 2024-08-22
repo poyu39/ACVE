1e8f1a7c46c53b00adb6a24f2918ba42a843e072
poyu39 <bruce920309@gmail.com>
2024-08-22 22:53:12 +0800

    feat: 新增 style

--- src/cli.py
+++ src/cli.py
@@ -10,7 +10,8 @@ print(f'Using {device} device')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Anime Character Voice Extractor')
-    parser.add_argument('--input', type=str, required=False, help='Path to the directory containing anime files')
+    parser.add_argument('--input', type=str, required=True, help='Path to the directory containing anime files')
+    parser.add_argument('--style', type=str, required=True, help='字幕檔中所要擷取的目標風格')
     parser.add_argument('--distance_limit', type=float, required=False)
     parser.add_argument('--test_distance', type=bool, default=False, required=False)
     args = parser.parse_args()
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     videos = None
     input_path = os.path.abspath(f'./../input/{args.input}')
     
-    videos = [os.path.join(file) for file in os.listdir(input_path) if file.split('.')[1] == 'mkv']
+    videos = [os.path.join(file) for file in os.listdir(input_path) if file.split('.')[1] in ['mkv', 'mp4']]
     
     acve = ACVE(args.input)
     for video in videos:
@@ -27,7 +28,7 @@ if __name__ == '__main__':
         ass_path = f"{input_path}/{video.split('.')[0]}.ass"
         ass = Ass(ass_path)
         ass.format_events()
-        dialogue_audios_path = acve.split_audio_by_dialogue(f'{output_path}/vocals.wav', ass.get_dialogues('TEXT-JP'))
-        acve.speaker_diarization(dialogue_audios_path, args.distance_limit, args.test_distance)
+        dialogue_audios_path = acve.split_audio_by_dialogue(f'{output_path}/vocals.wav', ass.get_dialogues(args.style))
+        # acve.speaker_diarization(dialogue_audios_path, args.distance_limit, args.test_distance)
         # rttm = acve.speaker_diarization(f'{output_path}/vocals.wav')
-        # acve.extract_speaker_audio(f'{output_path}/vocals.wav', rttm)
+        # acve.extract_speaker_audio(f'{output_path}/vocals.wav', rttm) 
